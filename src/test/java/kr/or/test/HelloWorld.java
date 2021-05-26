65583eaf6f5f536e9c9deaf98de97cc1a05f6aac
뱃살 현문 < @DESKTOP-R8AN91B>
2021-05-26 14:18:21 +0900

    26일 5교시

--- src/test/java/kr/or/test/HelloWorld.java
+++ src/test/java/kr/or/test/HelloWorld.java
@@ -4,7 +4,9 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// 자바앱은 진입 메서드가 필요, main()
+		//classpath->exclude test code unchecked 해야지만,오류 안남
 		System.out.println("헬로 자바");
+		
 	}
 
 }

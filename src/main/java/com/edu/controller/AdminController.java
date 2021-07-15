42f36cf232ea77fbb1a851c64e5e7e1a14a9ad1c
moon <moon@test.com>
2021-07-15 12:50:57 +0900

    7월 15일

--- src/main/java/com/edu/controller/AdminController.java
+++ src/main/java/com/edu/controller/AdminController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -280,7 +281,10 @@ public class AdminController {
 	
 	//아래경로는 회원 신규 등록을 처리하는 서비스를 호출하는 URL
 	@RequestMapping(value ="/admin/member/member_insert",method=RequestMethod.POST)
-	public String insertmember(PageVO pageVO,MemberVO memberVO)throws Exception{//인서트에서온값	
+	public String insertmember(HttpServletRequest request,MultipartFile file,PageVO pageVO,MemberVO memberVO)throws Exception{//인서트에서온값
+		if(!file.getOriginalFilename() .isEmpty()) {
+			commonUtil.profile_upload(memberVO.getUser_id(),request,file);
+		}
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		String rawPassword = memberVO.getUser_pw();//원시 패스워드값
 		String encPassword = passwordEncoder.encode(rawPassword);
